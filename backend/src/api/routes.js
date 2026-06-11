1a6f2b2b588da011bc02adc5aaa3a1bcb1466df3
EyobaGit <juu.shashe@gmail.com>
2026-06-11 23:06:20 +0300

    routes

--- backend/src/api/routes.js
+++ backend/src/api/routes.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import authRoutes from './auth/routes/auth.routes.js';
 import questionRoutes from './question/routes/question.routes.js';
-//import answersRoutes from './answer/routes/answer.routes.js';
+import answersRoutes from './answers/routes/answer.routes.js';
 
 export const mainRouter = express.Router();
 
@@ -10,4 +10,4 @@ mainRouter.use('/auth', authRoutes);
 // /api/questions
 mainRouter.use("/questions", questionRoutes);
 // /api/answers
-//mainRouter.use("/answers", answersRoutes);
+mainRouter.use("/answers", answersRoutes);
